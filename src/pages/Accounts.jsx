44551dd7a9974f21a812ffa96541931a34de606a
Nitesh Kumar <niteshkumar@Niteshs-MacBook-Air.local>
2025-07-14 16:47:49 +0530

    updated

--- src/pages/Accounts.jsx
+++ src/pages/Accounts.jsx
@@ -7,10 +7,10 @@ import TrendsChart from '../components/charts/TrendsChart';
 
 const Accounts = () => {
   return (
-    <div className="min-h-screen px-6 py-10 bg-white dark:bg-gray-900 px-10">
-      {/* top */}
+    <div className="min-h-screen px-4 sm:px-6 lg:px-10 py-10 bg-white dark:bg-gray-900">
+   
       <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-20 mt-10">
-    
+   
         <div>
           <h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-4">
             Evaluate performance of your ads on all levels
@@ -19,7 +19,7 @@ const Accounts = () => {
             Ads Manager is your actionable full-funnel view on both Apple Search Ads stats and post-install metrics to take educated actions & accelerate ROAS.
           </p>
 
-          <div className="flex gap-6 items-center">
+          <div className="flex gap-6 items-center flex-wrap">
             <Link
               to="#"
               className="inline-flex items-center gap-2 px-5 py-3 bg-orange-100 text-orange-600 font-medium rounded-lg shadow-sm hover:bg-orange-200 transition"
@@ -31,7 +31,7 @@ const Accounts = () => {
           </div>
         </div>
 
-       
+        
         <div>
           <div className="w-full aspect-video rounded-xl bg-orange-500 p-6 text-white flex items-center justify-center shadow-md">
             <div className="flex flex-col items-center text-center gap-4">
@@ -49,27 +49,22 @@ const Accounts = () => {
         </div>
       </div>
 
-      {/* metrics */}
+ 
       <div className="mt-16">
         <h2 className="text-xl font-semibold text-gray-800 dark:text-white mb-4">
           Total Summary
         </h2>
-        <div className="flex flex-wrap gap-4">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-4">
           {summaryData.map((item, index) => (
-            <div
-              key={index}
-              className="w-full sm:w-1/2 md:w-1/3 lg:w-1/5 xl:w-1/6"
-            >
-              <MetricCard {...item} />
-            </div>
+            <MetricCard key={index} {...item} />
           ))}
         </div>
       </div>
 
-      {/* chart */}
-      <div className='mt-20'>
-      <h1 className='font-bold mb-10 text-xl'>Chart</h1>
-        <TrendsChart/>
+ 
+      <div className="mt-20">
+        <h1 className="font-bold mb-10 text-xl">Chart</h1>
+        <TrendsChart />
       </div>
     </div>
   );

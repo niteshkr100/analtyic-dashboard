4307c19cc52b4c8a48eadef7b52ad0a6819ba660
Nitesh Kumar <niteshkumar@Niteshs-MacBook-Air.local>
2025-07-14 18:06:18 +0530

    updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -71,8 +71,6 @@ const Sidebar = ({  closeSidebar = () => {}, isDark, handleDark }) => {
     <span className="text-white font-medium">
       {isDark ? 'Dark' : 'Light'}
     </span>
-
- 
     <button
       onClick={handleDark}
       className={`w-10 h-5 flex items-center rounded-full p-1 transition-colors duration-300 ${
